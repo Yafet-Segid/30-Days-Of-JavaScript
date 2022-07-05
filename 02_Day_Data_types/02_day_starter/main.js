59d923ef907a2bd9bce2d28dd44d0645630f1cee
Your Name <you@example.com>
2022-07-04 22:44:05 -0500

    30 Days JavaScript

--- 02_Day_Data_types/02_day_starter/main.js
+++ 02_Day_Data_types/02_day_starter/main.js
@@ -1,1 +1,9 @@
-// this is your main.js script
+
+
+
+
+const love = 'Love is the best thing in this world. Some found their love and some are still looking for their love.' 
+
+const loveMatch = /love/gi;
+
+console.log(love.match(loveMatch))
